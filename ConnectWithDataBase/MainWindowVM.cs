2c13a0019e923bb64ed02f4dd1119ba609752f39
Bhanuka Anjana <85387483+Bhanu-2500@users.noreply.github.com>
2023-01-12 10:40:28 +0530

    Final

---
+++ ConnectWithDataBase/MainWindowVM.cs
@@ -0,0 +1,57 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectWithDataBase
+{
+    public partial class  MainWindowVM : ObservableObject
+    {
+        [ObservableProperty]
+        public string firstName;
+
+        [ObservableProperty]
+        public string lastName;
+
+        [ObservableProperty]
+        public int age;
+
+        [ObservableProperty]
+        ObservableCollection<Person> people;
+
+        [RelayCommand]
+        public void InsertPerson()
+        {
+            Person p = new Person()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Age = age
+            };
+            using ( var db = new PersonContext())
+            {
+                db.Persons.Add(p);
+                db.SaveChanges();
+            }
+        }
+
+        public void LoadPerson()
+        {
+            using(var db = new PersonContext())
+            {
+                var list = db.Persons.ToList();
+                People = new ObservableCollection<Person>(list);
+            }
+        }
+        public MainWindowVM()
+        {   
+            LoadPerson();
+
+        }
+    }
+
+}
---
+++ ConnectWithDataBase/PersonContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectWithDataBase
+{
+    public class PersonContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        private readonly string _path = @"C:\Users\bhanu\Desktop\ConnectWithDataBase\persons.db"; // <=If you put @ mark everything between in "" are going to be Raw string(\,/ neglected)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite($"Data Source={_path}");
+    }
+}
